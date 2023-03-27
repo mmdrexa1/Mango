c951fafef3e1ef7148d98967d03cfda2d41b6752
Arror <hallo.maqiang@icloud.com>
2023-03-27 16:20:48 +0800

    Add model

--- Mango/Setting/DNS/MGDNSModel.swift
+++ Mango/Setting/DNS/MGDNSModel.swift
@@ -6,6 +6,41 @@ extension MGConstant {
 
 public struct MGDNSModel: Codable, Equatable {
     
+    public enum QueryStrategy: String, Identifiable, CaseIterable, CustomStringConvertible, Codable {
+        public var id: Self { self }
+        case useIP      = "UseIP"
+        case useIPv4    = "UseIPv4"
+        case useIPv6    = "UseIPv6"
+        public var description: String {
+            return self.rawValue
+        }
+    }
+    
+    public enum Server: Codable, Equatable {
+        public struct Object: Codable, Equatable {
+            public var address: String = ""
+            public var port: Int = 0
+            public var domains: [String] = []
+            public var expectIPs: [String] = []
+            public var skipFallback: Bool = false
+            public var clientIP: String?
+        }
+        case string(String)
+        case object(Object)
+    }
+    
+    public var __osLocalDNS__: [String] = ["1.1.1.1"]
+    public var __enable__: Bool = false
+    
+    public var hosts: [String: [String]]?
+    public var servers: [Server]?
+    public var clientIp: String?
+    public var queryStrategy: QueryStrategy = .useIP
+    public var disableCache: Bool = false
+    public var disableFallback: Bool = false
+    public var disableFallbackIfMatch: Bool = false
+    public var tag: String = "dns-in"
+    
     public static let `default` = MGDNSModel()
     
     public static var current: MGDNSModel {
--- Mango/Setting/DNS/MGDNSViewModel.swift
+++ Mango/Setting/DNS/MGDNSViewModel.swift
@@ -2,28 +2,51 @@ import Foundation
 
 final class MGDNSViewModel: ObservableObject {
     
+    @Published var __osLocalDNS__: [String] = ["1.1.1.1"]
+    @Published var __enable__: Bool = false
     
+    @Published var hosts: [String: [String]]
+    @Published var servers: [MGDNSModel.Server]
+    @Published var clientIp: String
+    @Published var queryStrategy: MGDNSModel.QueryStrategy = .useIP
+    @Published var disableCache: Bool = false
+    @Published var disableFallback: Bool = false
+    @Published var disableFallbackIfMatch: Bool = false
+    @Published var tag: String = "dns-in"
     
     init() {
-//        let model = MGDNSModel.current
-        
+        let model = MGDNSModel.current
+        self.__osLocalDNS__ = model.__osLocalDNS__
+        self.__enable__ = model.__enable__
+        self.hosts = model.hosts ?? [:]
+        self.servers = model.servers ?? []
+        self.clientIp = model.clientIp ?? ""
+        self.queryStrategy = model.queryStrategy
+        self.disableCache = model.disableCache
+        self.disableFallback = model.disableFallback
+        self.disableFallbackIfMatch = model.disableFallbackIfMatch
     }
     
     func save(updated: () -> Void) {
-//        do {
-//            fatalError()
-//            let model = MGRouteModel(
-//                domainStrategy: self.domainStrategy,
-//                domainMatcher: self.domainMatcher,
-//                rules: self.rules
-//            )
-//            guard model != .current else {
-//                return
-//            }
-//            UserDefaults.shared.set(try JSONEncoder().encode(model), forKey: MGConstant.route)
-//            updated()
-//        } catch {
-//            fatalError(error.localizedDescription)
-//        }
+        do {
+            let model = MGDNSModel(
+                __osLocalDNS__: self.__osLocalDNS__,
+                __enable__: self.__enable__,
+                hosts: self.hosts.isEmpty ? nil : self.hosts,
+                servers: self.servers.isEmpty ? nil : self.servers,
+                clientIp: self.clientIp.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ? nil : self.clientIp,
+                queryStrategy: self.queryStrategy,
+                disableCache: self.disableCache,
+                disableFallback: self.disableFallback,
+                disableFallbackIfMatch: self.disableFallbackIfMatch
+            )
+            guard model != .current else {
+                return
+            }
+            UserDefaults.shared.set(try JSONEncoder().encode(model), forKey: MGConstant.dns)
+            updated()
+        } catch {
+            fatalError(error.localizedDescription)
+        }
     }
 }
