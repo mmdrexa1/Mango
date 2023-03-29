ba6d922de78189a91a1caa2321d2d1627c734e7e
Arror <hallo.maqiang@icloud.com>
2023-03-29 17:45:14 +0800

    Update

--- Mango/Setting/Outbound/MGOutboundEntranceView.swift
+++ Mango/Setting/Outbound/MGOutboundEntranceView.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 struct MGOutboundEntranceView: View {
     
+    @StateObject private var outboundViewModel = MGConfigurationPersistentViewModel<MGConfiguration.Outbound>()
+
+    
     var body: some View {
         NavigationLink {
-            MGOutboundView()
+            MGOutboundView(outboundViewModel: outboundViewModel)
         } label: {
             Label("Outbound", systemImage: "square.and.arrow.up")
         }
--- Mango/Setting/Outbound/MGOutboundView.swift
+++ Mango/Setting/Outbound/MGOutboundView.swift
@@ -2,9 +2,87 @@ import SwiftUI
 
 struct MGOutboundView: View {
     
+    @ObservedObject private var outboundViewModel: MGConfigurationPersistentViewModel<MGConfiguration.Outbound>
+    
+    init(outboundViewModel: MGConfigurationPersistentViewModel<MGConfiguration.Outbound>) {
+        self._outboundViewModel = ObservedObject(initialValue: outboundViewModel)
+    }
+    
     var body: some View {
-        Form {}
-        .onDisappear {}
+        Form {
+            Section("Direct") {
+                LabeledContent("Domain Strategy") {
+                    Picker("", selection: $outboundViewModel.model.freedom.settings.domainStrategy) {
+                        ForEach(MGConfiguration.Outbound.FreedomSettings.DomainStrategy.allCases) { ds in
+                            Text(ds.description)
+                        }
+                    }
+                    .labelsHidden()
+                    .fixedSize()
+                }
+                LabeledContent("Redirect") {
+                    TextField("", text: Binding(get: {
+                        outboundViewModel.model.freedom.settings.redirect ?? ""
+                    }, set: { value in
+                        let reval = value.trimmingCharacters(in: .whitespacesAndNewlines)
+                        outboundViewModel.model.freedom.settings.redirect = reval.isEmpty ? nil : reval
+                    }))
+                }
+            }
+            Section("Block") {
+                LabeledContent("Response Type") {
+                    Picker("", selection: $outboundViewModel.model.blackhole.settings.response.type) {
+                        ForEach(MGConfiguration.Outbound.BlackholeSettings.ResponseType.allCases) { rt in
+                            Text(rt.description)
+                        }
+                    }
+                    .labelsHidden()
+                    .fixedSize()
+                }
+            }
+            Section("DNS") {
+                LabeledContent("Network") {
+                    Picker("", selection: $outboundViewModel.model.dns.settings.network) {
+                        ForEach(MGConfiguration.Outbound.DNSSettings.Network.allCases) { nw in
+                            Text(nw.description)
+                        }
+                    }
+                    .labelsHidden()
+                    .fixedSize()
+                }
+                LabeledContent("Address") {
+                    TextField("", text: Binding(get: {
+                        outboundViewModel.model.dns.settings.address ?? ""
+                    }, set: { value in
+                        let reval = value.trimmingCharacters(in: .whitespacesAndNewlines)
+                        outboundViewModel.model.dns.settings.address = reval.isEmpty ? nil : reval
+                    }))
+                }
+                LabeledContent("Port") {
+                    TextField("", text: Binding(get: {
+                        outboundViewModel.model.dns.settings.port.flatMap({ "\($0)" }) ?? ""
+                    }, set: { value in
+                        let reval = value.trimmingCharacters(in: .whitespacesAndNewlines)
+                        outboundViewModel.model.dns.settings.port = Int(reval)
+                    }))
+                    .keyboardType(.numberPad)
+                }
+            }
+            Section("Order") {
+                ForEach(outboundViewModel.model.order) { tag in
+                    Text(tag.description)
+                }
+                .onMove { from, to in
+                    outboundViewModel.model.order.move(fromOffsets: from, toOffset: to)
+                }
+            }
+        }
+        .onDisappear {
+            outboundViewModel.save()
+        }
+        .lineLimit(1)
+        .multilineTextAlignment(.trailing)
+        .scrollDismissesKeyboard(.interactively)
         .navigationTitle(Text("Outbound"))
         .navigationBarTitleDisplayMode(.large)
         .environment(\.editMode, .constant(.active))
